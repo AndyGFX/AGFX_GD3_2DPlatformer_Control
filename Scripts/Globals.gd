37f072d65cde96dd0d3aba47b627944ee4a7c797
AndyGFX <o.kollar@gmail.com>
2018-10-13 15:17:09 +0200

    Movement control added

---
+++ Scripts/Globals.gd
@@ -0,0 +1,39 @@
+extends Node
+
+# Movement facing state
+enum eFacing { TO_LEFT, TO_RIGHT}
+
+enum eDirection { IDLE = 0,TO_LEFT = -1, TO_RIGHT=1, TO_UP = -1, TO_DOWN = 1 }
+
+# Action state
+#enum eActionState { FIRE, THROW, STAB, USE }
+
+# Animation state
+enum eAnimState { IDLE, WALK, JUMP, FALL, DIE, HURT, CLIMB, OBSTACLE, CRUNCH, CRUNCHWALK }
+
+# screen transition mode
+enum eTransitionMode { NONE,TO_BLACK,TO_TRANSPARENT}
+
+# ---------------------------------------------------------------------------
+# GAME data
+# ---------------------------------------------------------------------------
+var coins = 0;
+var health = 0;
+var items = {};
+
+# ---------------------------------------------------------------------------
+# GAME prefabs 
+# ---------------------------------------------------------------------------
+
+# teleport button info
+
+# powerups
+
+# bullet prefab for firing
+
+# Sound FX player
+
+# ---------------------------------------------------------------------------
+# GAME Levels
+# ---------------------------------------------------------------------------
+
