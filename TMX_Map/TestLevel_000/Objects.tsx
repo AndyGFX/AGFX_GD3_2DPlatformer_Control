a5b29f47c4fd337cef5287fa2895113aa8a0c6e1
AndyGFX <o.kollar@gmail.com>
2019-02-23 22:05:55 +0100

    Prepare data for build items fromtiled files

---
+++ TMX_Map/TestLevel_000/Objects.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2018.11.13" name="Objects" tilewidth="16" tileheight="16" tilecount="32" columns="4">
+ <image source="Objects.png" width="64" height="128"/>
+ <tile id="0" type="ITEM-Health"/>
+ <tile id="4" type="ITEM-Ammo"/>
+ <tile id="8" type="ITEM-Diamod"/>
+ <tile id="12" type="ITEM-Grenade"/>
+ <tile id="20" type="MSG-Panel"/>
+ <tile id="24" type="Door"/>
+ <tile id="28" type="DoorWithKey"/>
+</tileset>
