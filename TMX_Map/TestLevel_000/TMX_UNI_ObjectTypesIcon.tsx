f07734b24d1b752464170e71ab70734f42f8b348
AndyGFX <o.kollar@gmail.com>
2019-03-03 08:45:50 +0100

    New UNI scene object icons

---
+++ TMX_Map/TestLevel_000/TMX_UNI_ObjectTypesIcon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="2019.02.10" name="TMX_UNI_ObjectTypesIcon" tilewidth="16" tileheight="16" tilecount="320" columns="10">
+ <image source="TMX_UNI_ObjectTypesIcon.png" width="160" height="512"/>
+</tileset>
